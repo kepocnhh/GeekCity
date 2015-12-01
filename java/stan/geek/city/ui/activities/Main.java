e5d8df7d66eb8ef47b319200f0343e961ed80241
antonoctopod <a.grechushnikov@octopod.com>
2015-12-01 15:04:28 +0300

    init AndroidStudio project for geekcity.ru

---
+++ java/stan/geek/city/ui/activities/Main.java
@@ -0,0 +1,32 @@
+package stan.geek.city.ui.activities;
+
+import stan.geek.city.R;
+
+public class Main
+        extends StanActivity
+{
+    //_______________VIEWS
+
+    //_______________FIELDS
+
+    public Main()
+    {
+        super(R.layout.main, R.id.main_frame);
+    }
+
+    @Override
+    protected void initFragments()
+    {
+
+    }
+
+    @Override
+    protected void initViews()
+    {
+    }
+
+    @Override
+    protected void init()
+    {
+    }
+}
